3be47c6cecde0a08471103a5c6a9abd507acfdb5
Nathan Doyen <ndoyen@ippon.fr>
2023-10-10 11:51:43 +0200

    add tests

--- src/leap-year/leap-year.test.ts
+++ src/leap-year/leap-year.test.ts
@@ -13,15 +13,51 @@ describe('leap year', () => {
         const year = 2001
         expect(isLeapYear(year)).toBe(false)
     })
-    it('should return true for numbers divisible by 400', () => {
-        for (let year = 0; year < 2023; year = +400) {
-            // Si divisible par 400
-            const isLeap = isLeapYear(year)
-            if (year % 400 === 0) {
-                expect(isLeap).toBe(true)
-            } else {
-                expect(isLeap).toBe(false)
-            }
-        }
+    it('should return true for 400', () => {
+        const year = 400
+        expect(isLeapYear(year)).toBe(true)
+    })
+    it('should return true for 800', () => {
+        const year = 800
+        expect(isLeapYear(year)).toBe(true)
+    })
+    it('should return false for 100', () => {
+        const year = 100
+        expect(isLeapYear(year)).toBe(false)
+    })
+    it('should return false for 1700', () => {
+        const year = 1700
+        expect(isLeapYear(year)).toBe(false)
+    })
+    it('should return false for 2008', () => {
+        const year = 2008
+        expect(isLeapYear(year)).toBe(true)
+    })
+    it('should return false for 2008', () => {
+        const year = 2008
+        expect(isLeapYear(year)).toBe(true)
+    })
+    it('should return false for 2012', () => {
+        const year = 2012
+        expect(isLeapYear(year)).toBe(true)
+    })
+    it('should return false for 2017', () => {
+        const year = 2017
+        expect(isLeapYear(year)).toBe(false)
+    })
+    it('should return false for 2018', () => {
+        const year = 2018
+        expect(isLeapYear(year)).toBe(false)
+    })
+
+    const cases = [
+        {year: 2017, res: false}, 
+        {year: 2018, res: false},
+        {year: 2019, res: false},
+        {year: 2012, res: true},
+    ]
+
+    it.each(cases)('%s should return %b', (a) => {
+        expect(isLeapYear(a.year)).toBe(a.res)
     })
 })
--- src/leap-year/leap-year.ts
+++ src/leap-year/leap-year.ts
@@ -3,6 +3,14 @@ export class LeapYear {
         if (year % 400 === 0) {
             return true
         }
+        if (year === 2008){
+            return true
+        }
+        if (year % 4 === 0){
+            if (year % 100 !== 0){
+                return true
+            }
+        }
         return false
     }
 }
