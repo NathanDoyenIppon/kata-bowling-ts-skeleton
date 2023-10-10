8854bd5b1d60fae34b357eeb4114e3561ae2a62e
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2023-10-10 16:12:25 +0200

    fin loic

--- src/bowling-game/bowling-game.test.ts
+++ src/bowling-game/bowling-game.test.ts
@@ -1,4 +1,4 @@
-import {BowlingGame} from './bowling-game'
+import { BowlingGame } from './bowling-game'
 
 describe('bowling line', () => {
     it('should be true if installation work', () => {
@@ -6,35 +6,45 @@ describe('bowling line', () => {
     })
     it('score should return 0 at the beginning of the line', () => {
         const line = new BowlingGame()
-
+        for (let frame = 1; frame <= 10; frame++) {
+            line.frame('-')
+        }
         expect(line.score()).toEqual(0)
     })
     it('score should be total of the first frame', () => {
         const line = new BowlingGame()
 
-        line.frame(3)
-
+        line.frame('21')
+        for (let frame = 2; frame <= 10; frame++) {
+            line.frame('-')
+        }
         expect(line.score()).toEqual(3)
     })
     it('score should be total of 10 if first frame is strike', () => {
         const line = new BowlingGame()
 
         line.frame('X')
-
+        for (let frame = 2; frame <= 10; frame++) {
+            line.frame('-')
+        }
         expect(line.score()).toEqual(10)
     })
     it('score should be total of 10 if first frame is spare', () => {
         const line = new BowlingGame()
 
         line.frame('7/')
-
+        for (let frame = 2; frame <= 10; frame++) {
+            line.frame('-')
+        }
         expect(line.score()).toEqual(10)
     })
     it('score should add the number of pins touched', () => {
         const line = new BowlingGame()
 
         line.frame('5-')
-
+        for (let frame = 2; frame <= 10; frame++) {
+            line.frame('-')
+        }
         expect(line.score()).toEqual(5)
     })
     it('score should remain the sameif no pins are touched', () => {
@@ -42,7 +52,23 @@ describe('bowling line', () => {
 
         line.frame('5-')
         line.frame('--')
-
+        for (let frame = 3; frame <= 10; frame++) {
+            line.frame('-')
+        }
         expect(line.score()).toEqual(5)
+    })
+    it('should double score if spare before', () => {
+        const line = new BowlingGame()
+        // score = 10
+        line.frame('/')
+
+        // score = 10
+        line.frame('5-')
+
+        // tot prochains lancers: 40
+        for (let frame = 3; frame <= 10; frame++) {
+            line.frame('5-')
+        }
+        expect(line.score()).toEqual(60)
     })
 })
--- src/bowling-game/bowling-game.ts
+++ src/bowling-game/bowling-game.ts
@@ -1,22 +1,36 @@
 export class BowlingGame {
     totalScore: number
+    coefRow1: number
+    coefRow2: number
     constructor() {
         this.totalScore = 0
+        this.coefRow1 = 1
+        this.coefRow2 = 1
     }
-    frame(numberOfPins: string | number): void {
-        if (typeof numberOfPins === "string"){
-            if (numberOfPins === 'X'){
-                this.totalScore += 10
-            }
-            if (numberOfPins.includes('/')){
-                this.totalScore += 10
-            }
-            if (numberOfPins.includes('-')){
-                this.totalScore += Number(numberOfPins.split('-')[0])
-            }
-        } else {
-            this.totalScore += numberOfPins
+    initializeSpareAndStrike() {
+        this.coefRow1 = 1
+        this.coefRow2 = 1
+    }
+
+    frame(numberOfPins: string): void {
+        if (numberOfPins === 'X') {
+            this.totalScore += 10
+            return
         }
+        if (numberOfPins.includes('/')) {
+            this.totalScore += 10
+            this.coefRow1 = 2
+            return
+        }
+        if (numberOfPins.includes('-')) {
+            this.totalScore +=
+                Number(numberOfPins.split('-')[0]) * this.coefRow1 +
+                Number(numberOfPins.split('-')[1])
+            this.initializeSpareAndStrike()
+            return
+        }
+        let [roll1, roll2] = numberOfPins
+        this.totalScore += Number(roll1) * this.coefRow1 + Number(roll2)
     }
 
     score(): number {
